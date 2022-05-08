b6f12ece6fdca99f0d5f775b3c494a0877faefe6
marutaro1 <usersrecord@gmail.com>
2022-05-09 08:49:54 +0900

    update login

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -36,6 +37,9 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+            if ($exception instanceof TokenMismatchException) {		
+                return redirect('/login');					
+            }	
         });
     }
     
