62185bcee7db98eee41e6c36ceb9bf9ee3c57e47
marutaro1 <usersrecord@gmail.com>
2022-05-10 10:44:44 +0900

    redirect login

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -34,8 +35,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        
+        // 追加2
+        //エラー画面をユーザーに見せる必要はないので、ログイン画面にリダイレクトさせる
+        if ($exception instanceof TokenMismatchException) {
+            return redirect('/login');
+        }
+
+        return parent::render($request, $exception);
     }
+    
     public function register()
     {
         $this->reportable(function (Throwable $e) {
