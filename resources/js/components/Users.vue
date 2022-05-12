4dd85d0c564a00161d4626605b56581901e2b2fd
marutaro1 <usersrecord@gmail.com>
2022-05-12 09:06:11 +0900

    update serch_number test_check

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -166,15 +166,7 @@
                        this.test = '空';
                        factoryuser_array.push(factoryuserData);
                     }
-                    else if (factoryuserData.factoryuser_name.indexOf(this.keyword) !== -1 &&
-                        room_number === '1' && Number(factoryuserData.number) <= 10000 &&
-                        room_number.indexOf(this.floorKeyword) !== -1 &&
-                        factoryuserData.care_level.indexOf(this.serchCareLevelKeyword) !== -1
-                    )  {
-                         this.test = '1';
-                         factoryuser_array.push(factoryuserData);
-
-                   }
+                    
                     else if (factoryuserData.factoryuser_name.indexOf(this.keyword) !== -1 &&
                         room_number === this.floorKeyword &&
                         this.floorKeyword !== '1' &&
