0ca78d8276c4903956c5c6b5ec61252f70769de9
marutaro1 <usersrecord@gmail.com>
2022-05-13 08:14:46 +0900

    delete test button

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="mt-2 mx-3" @mousemove.once="getFactoryusers">
     <h2>利用者一覧</h2>
-    <button @click="deleteTest">delete</button>
     <label class="col-5 col-form-label">キーワード検索: </label>
     <div class="col-5">
       <input
